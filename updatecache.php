8fec48b0968f6dd902f35a64d5397dcf02ad3c94
Jille Timmermans <jille@karpenoktem.nl>
2011-05-11 18:45:55 +0000

    updatecache: Foto's zonder iets (!invalidated) voorrang geven

--- updatecache.php
+++ updatecache.php
@@ -2,7 +2,7 @@
 	$cli_mode = true;
 	require('header.php');
 
-	$res = mysql_query("SELECT * FROM fa_photos WHERE ((NOT FIND_IN_SET('thumb', cached) OR NOT FIND_IN_SET('large', cached)) OR FIND_IN_SET('invalidated', cached)) AND visibility IN('hidden', 'leden', 'world') ORDER BY RAND()");
+	$res = mysql_query("SELECT * FROM fa_photos WHERE ((NOT FIND_IN_SET('thumb', cached) OR NOT FIND_IN_SET('large', cached)) OR FIND_IN_SET('invalidated', cached)) AND visibility IN('hidden', 'leden', 'world') ORDER BY FIND_IN_SET('invalidated', cached), RAND()");
 	while($row = mysql_fetch_assoc($res)) {
 		echo '==> '. $row['path'] . $row['name'] ."\n";
 		if(!is_dir($cachedir . $row['path'])) {
