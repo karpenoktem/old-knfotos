8c2acea5ba67eb9fb70133398dff547365d2a56e
Jille Timmermans <jille@karpenoktem.nl>
2011-05-28 12:05:06 +0000

    var_dump()'s weg

--- service.php
+++ service.php
@@ -1,18 +1,15 @@
 <?php
 	require('header.php');
 
-	var_dump($_POST);
 	if(isAdmin() && isset($_POST['updatePhoto'], $_POST['visibility'], $_POST['rotation'], $_POST['tags'])) {
 		if(!updatePhotoMetadata($_POST['updatePhoto'], $_POST['visibility'], $_POST['rotation'], $_POST['tags'])) {
 			echo "Boem :(\n";
 		}
-		var_dump(mysql_error());
 		die(":)");
 	} elseif(isAdmin() && isset($_POST['updateAlbum'], $_POST['visibility'], $_POST['humanname'])) {
 		if(!updateAlbumMetadata($_POST['updateAlbum'], $_POST['visibility'], $_POST['humanname'])) {
 			echo "Boem :(\n";
 		}
-		var_dump(mysql_error());
 		die(":)");
 	}
 	die('ERR');
