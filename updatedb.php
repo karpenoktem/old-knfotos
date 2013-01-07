820965ebd004acbcb046c1d0183c32404a316ec7
Ayke van Laethem <aykevanlaethem@gmail.com>
2013-01-07 21:17:17 +0100

    improved updatedb.php (mainly clean-up)
    * Added comments and description
    * Changed some variable names
    * removed if (true || ...) {

--- updatedb.php
+++ updatedb.php
@@ -2,12 +2,20 @@
 	$cli_mode = true;
 	require('header.php');
 
+	/* This script updates the database with photos in the $fotodir
+	   It sets old albums/photos to lost, sets re-found albums/photos to hidden
+	   and inserts new albums/photos with visibility hidden.
+	 */
+
 	$albums = array();
 	$photos = array();
 	scan_gallery('');
+	// now the $albums and $photos contain all albums/photos in the $fotodir
 
+	// check each album
 	$res = sql_query('SELECT name, path, visibility FROM fa_albums');
 	while($row = mysql_fetch_assoc($res)) {
+		// if the album in the db doesn't exist anymore, set it to lost.
 		if(!isset($albums[$row['path']]) || !in_array($row['name'], $albums[$row['path']])) {
 			sql_query("UPDATE fa_albums SET visibility='lost' WHERE name=%s AND path=%s", $row['name'], $row['path']);
 		} else {
@@ -17,7 +25,9 @@
 			}
 		}
 	}
+	// $albums now only contains new albums (not yet in the db)
 
+	// insert new albums
 	foreach($albums as $path=>$dirs) {
 		foreach($dirs as $album) {
 			if(isset($photos[$path . $album .'/']) || isset($albums[$path . $album .'/'])) {
@@ -26,8 +36,10 @@
 		}
 	}
 
+	// check each photo
 	$res = sql_query('SELECT name, path, visibility FROM fa_photos');
 	while($row = mysql_fetch_assoc($res)) {
+		// if the photo in the db isn't in $fotodir anymore, set it to lost
 		if(!isset($photos[$row['path']]) || !in_array($row['name'], $photos[$row['path']])) {
 			sql_query("UPDATE fa_photos SET visibility='lost' WHERE name=%s AND path=%s", $row['name'], $row['path']);
 		} else {
@@ -37,6 +49,7 @@
 			}
 		}
 	}
+	// $photos now only contains new photos
 
 	// Insert the remaining images
 	foreach($photos as $path=>$dirs) {
@@ -61,29 +74,34 @@
 			}
 
 			// Insert image into database
-			sql_query("INSERT INTO fa_photos (
+			if (!sql_query("INSERT INTO fa_photos (
 					name,
 					path,
 					rotation)
 				VALUES (%s, %s, %i)",
-					$photo, $path, $or);
+					$photo, $path, $or))
+				die (mysql_error());
 		}
 	}
 	require('footer.php');
 
+	/* This recursively scans the $fotodir and stores the results in $albums and $photos */
 	function scan_gallery($path) {
 		global $fotodir, $albums, $photos, $extensions;
+		echo 'scanning '.$path."\n";
 
-		foreach(scandir($fotodir . $path) as $f) {
-			if($f[0] == '.') {
+		foreach(scandir($fotodir . $path) as $fn) {
+			if($fn[0] == '.') {
 				continue;
 			}
-			if(true || is_readable($fotodir . $path . $f)) {
-				if(is_dir($fotodir . $path . $f)) {
-					$albums[$path][] = $f;
-					scan_gallery($path . $f .'/');
-				} elseif(isset($extensions[getext($f)]) && $path != '') {
-					$photos[$path][] = $f;
+			$subpath = $fotodir . $path . $fn;
+			if(is_readable($subpath)) {
+				if(is_dir($subpath)) {
+					$albums[$path][] = $fn;
+					scan_gallery($path . $fn .'/');
+				} elseif(isset($extensions[strtolower(getext($fn))]) && $path != '') {
+					echo 'found photo: '.$subpath."\n";
+					$photos[$path][] = $fn;
 				}
 			}
 		}
