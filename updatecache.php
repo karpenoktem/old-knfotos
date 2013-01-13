c986a56ce938db450421a1564de570f29daf7efa
Ayke van Laethem <aykevanlaethem@gmail.com>
2013-01-13 19:18:23 +0100

    Fixed audio bitrate issue (was too big)

--- updatecache.php
+++ updatecache.php
@@ -115,7 +115,7 @@
 	function transcode($input, $output, $bitrate, $size) {
 		global $ffmpeg;
 		// FUTURE TODO: remove '-strict experimental' (this is not needed in at least ffmpeg 1.1)
-		$command = $ffmpeg .' -i '. escapeshellarg($input) .' -strict experimental -b:v '. $bitrate .' -vf "scale=-1:'. $size .'" -y '. escapeshellarg($output);
+		$command = $ffmpeg .' -i '. escapeshellarg($input) .' -strict experimental -ab 128k -b:v '. $bitrate .' -vf "scale=-1:'. $size .'" -y '. escapeshellarg($output);
 		passthru($command, $ret);
 		if($ret != 0) {
 			echo "ERROR while transcoding via $command\n";
